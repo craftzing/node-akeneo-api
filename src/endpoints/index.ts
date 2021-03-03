50d9db0f4787ec1098d4e5b63cd143d3947c9ca9
jensdev <jens.verbeken@gmail.com>
2021-03-03 13:08:39 +0000

    Deploying to gh-pages from @ craftzing/node-akeneo-api@65a4ebc58b4ae109016998060975527bb0409103 🚀

--- src/endpoints/index.ts
+++
@@ -1,7 +0,0 @@
-export * as assetFamily from './asset-family';
-export * as attribute from './attribute';
-export * as category from './category';
-export * as family from './family';
-export * as productModel from './product-model';
-export * as product from './product';
-export * as referenceEntity from './reference-entity';
